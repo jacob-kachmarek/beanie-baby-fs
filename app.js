f75d5ed7ab4c84564c47f9b094bc3688eb9c5665
Jacob Kachmarek <jacob.kachmarek@gmail.com>
2023-01-20 14:03:42 -0800

    made display beani babies function to loop through data, create elements and append those to the dom

--- app.js
+++ app.js
@@ -1,11 +1,36 @@
 /* Imports */
+import { getBeanieBabies, getAstroSign } from './fetch-utils.js';
 
 /* Get DOM Elements */
+const beanieBabiesEl = document.querySelector('.beanie-babies');
+const selectEl = document.querySelector('select');
+const buttonEl = document.querySelector('button');
+const formEl = document.querySelector('form');
 
 /* State */
+let beanieBabiesData = [];
 
 /* Events */
+window.addEventListener('load', async () => {
+    const beanie = await getBeanieBabies();
+    beanieBabiesData = beanie;
+    displayBeanieBabies();
+    const astro = await getAstroSign();
+});
 
 /* Display Functions */
+function displayBeanieBabies() {
+    beanieBabiesEl.textContent = '';
+    for (let beanieBaby of beanieBabiesData) {
+        const beanieBabyEl = document.createElement('div');
+        const infoEl = document.createElement('p');
+        const imgEl = document.createElement('img');
+        infoEl.textContent = `Name: ${beanieBaby.title}`;
+        imgEl.src = beanieBaby.image;
+
+        beanieBabyEl.append(infoEl, imgEl);
+        beanieBabiesEl.append(beanieBabyEl);
+    }
+}
 
 // (don't forget to call any display functions you want to run on page load!)
--- fetch-utils.js
+++ fetch-utils.js
@@ -1,6 +1,14 @@
-const SUPABASE_URL = "https://gxwgjhfyrlwiqakdeamc.supabase.co";
+const SUPABASE_URL = 'https://gxwgjhfyrlwiqakdeamc.supabase.co';
 const SUPABASE_KEY =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNjQxMTMxMiwiZXhwIjoxOTUxOTg3MzEyfQ.PHekiwfLxT73qQsLklp0QFEfNx9NlmkssJFDnlvNIcA";
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNjQxMTMxMiwiZXhwIjoxOTUxOTg3MzEyfQ.PHekiwfLxT73qQsLklp0QFEfNx9NlmkssJFDnlvNIcA';
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
+export async function getBeanieBabies() {
+    let { data, error } = await client.from('beanie_babies').select('*').limit('100');
+    return data;
+}
+export async function getAstroSign() {
+    let { data, error } = await client.from('beanie_baby_astro_signs').select('*');
+    return data;
+}
